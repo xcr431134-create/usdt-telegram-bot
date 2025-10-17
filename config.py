3d91a9de915b4907c84f18f8d19c6f5f11909b18
xcr431134-create <xcr431134@gmail.com>
2025-10-17 19:26:46 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+import os
+
+BOT_TOKEN = os.getenv('BOT_TOKEN', '7973697789:AAFXfYXTgYaTAF1j7IGhp2kiv-kxrN1uImk')
+ADMIN_IDS = [int(os.getenv('ADMIN_ID', '8400225549'))]
+DATA_FILE = "users_data.json"
